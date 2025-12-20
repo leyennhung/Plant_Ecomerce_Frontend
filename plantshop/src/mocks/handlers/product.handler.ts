41f80005efb5575e9f8f0bfe3235ff6e6df4b999
Winniee79 <22130199@st.hcmuaf.edu.vn>
2025-12-20 23:00:47 +0700

    refactor product list in page home

--- plantshop/src/mocks/handlers/product.handler.ts
+++ plantshop/src/mocks/handlers/product.handler.ts
@@ -1,8 +1,9 @@
-import products from "../data/products.json";
-import { http, HttpResponse } from "msw";
+import data from "../data/products.json";
+import { http, HttpResponse } from "msw"; //hàm có sẵn của thư viện MSW
 
 export const productHandlers = [
+    //GET API
     http.get("/api/products", () => {
-        return HttpResponse.json(products);
+        return HttpResponse.json(data.products); //Trả về response dạng JSON
     })
 ];
--- plantshop/src/services/product.service.ts
+++ plantshop/src/services/product.service.ts
@@ -1,8 +1,13 @@
 import { api } from "./api";
 import type { Product } from "../types/product.type";
 
-export const productService = {
+//Khai báo
+export const productService = { // tự đăt để gom các hàm liên quan đến API
+    // Đặt hàm getAll() trả về một (kiểu) Promise bên trong là 1 mảng Product
     getAll(): Promise<Product[]> {
+        // Sau đó trả về res(response tự đặt) từ server
         return api.get("/products").then(res => res.data);
+        // data là thuộc tính có sẵn của axios response
+        // Chứa dữ liệu backend trả về
     }
 };
