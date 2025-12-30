06be6f340c587e8a3ec617e3bf1ce13af653daf1
Winniee79 <22130199@st.hcmuaf.edu.vn>
2025-12-30 20:54:14 +0700

    feat json product, update component productCard

--- plantshop/src/components/common/product/ProductCard.tsx
+++ plantshop/src/components/common/product/ProductCard.tsx
@@ -1,20 +1,36 @@
-import type {Product} from "../../../types/product.type.ts";
-import {formatPrice} from "../../../utils/formatPrice.ts";
+import type { Product } from "../../../types/product.type.ts";
+import { formatPrice } from "../../../utils/formatPrice.ts";
 import styles from "./ProductCard.module.css";
 
-type Props = { product: Product; };
+type Props = { product: Product };
 
 const ProductCard = ({ product }: Props) => {
+    const salePrice = product.salePrice ?? null;
+    const hasSale =
+        typeof salePrice === "number" && salePrice > 0 && salePrice < product.price;
+
     return (
         <div className={styles.card}>
             <img src={product.image}
                 alt={product.name}
                 className={styles.image}/>
-
             <h3 className={styles.name}>{product.name}</h3>
 
             <p className={styles.price}>
-                {formatPrice(product.price)}
+                {hasSale ? (
+                    <>
+            <span className={styles.originalPrice}>
+              {formatPrice(product.price)}
+            </span>
+                        <span className={styles.salePrice}>
+              {formatPrice(salePrice as number)}
+            </span>
+                    </>
+                ) : (
+                    <span className={styles.onlyPrice}>
+            {formatPrice(product.price)}
+          </span>
+                )}
             </p>
 
             <button className={styles.buyBtn}>Mua ngay</button>
